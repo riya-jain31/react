07d0641be42d3a3582d6acaceca172a4938cc059
Riyashaileshjain <riyajain3112003@gmail.com>
2025-03-21 22:56:47 +0530

    Todo-app completed

---
+++ todo-app/src/App.jsx
@@ -0,0 +1,82 @@
+import { useState } from 'react'
+import './App.css'
+
+
+function App() {
+  const [todos, setTodos] = useState([]);
+  const [todoInput, setTodoInput] = useState("");
+
+  function addTodo() {
+    if (todoInput == "") {
+      alert("Enter valid todo");
+      return;
+    }
+
+    const newTodo = {
+      title: todoInput,
+      completed: false,
+      id: Date.now(),
+    };
+
+    const updateArray = [...todos, newTodo];
+
+    setTodos(updateArray);
+
+    setTodoInput("");
+  }
+
+  function deleteTodo(id) {
+    const updatedArray = todos.filter((todo) => {
+      return todo.id != id;
+    });
+
+    setTodos(updatedArray);
+  }
+
+  function toggleComplete(id) {
+    const updateArray = todos.map((todo) => {
+      if (id == todo.id) {
+        todo.completed = !todo.completed;
+      }
+      return todo;
+    });
+
+    setTodos(updateArray);
+  }
+
+  return (
+    <>
+      <h1>Todo App</h1>
+
+      <div className="todos-input-wrapper">
+        <input
+          type="text"
+          id="todo-input"
+          onChange={(event) => setTodoInput(event.target.value)}
+          value={todoInput}
+        />
+        <button onClick={addTodo}>Add Todo</button>
+      </div>
+
+      <div className="todos-wrapper">
+        {todos.map((todo) => {
+          return (
+            <div
+              key={todo.id}
+              className={`todo ${todo.completed ? "completed" : "incomplete"}`}
+            >
+              <p>{todo.title}</p>
+              <button onClick={() => toggleComplete(todo.id)}>
+                {todo.completed ? "Mark as incomplete" : "Mark as complete"}
+              </button>
+              <button onClick={() => deleteTodo(todo.id)}>Delete</button>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+
+export default App
